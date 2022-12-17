bf366149602c732bc6be1340fe70d6aae90735aa
Roman Burko <redstone.kem@gmail.com>
2022-12-17 10:35:35 +0300

    updated home page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,37 +21,48 @@
         <header class="fixed w-full">
             <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
                 <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                    <a href="https://php-task-manager-ru.hexlet.app/" class="flex items-center">
+                    <a href="{{ route('home') }}" class="flex items-center">
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Менеджер
                             задач</span>
                     </a>
 
                     <div class="flex items-center lg:order-2">
-                        <a href="https://php-task-manager-ru.hexlet.app/login"
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <a :href="route('logout')"
+                                onclick="event.preventDefault();
+                                                this.closest('form').submit();"
+                                class="block bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white font-bold py-2 px-4 rounded ml-2">
+                                {{ __('Выход') }}
+                            </a>
+                        </form>
+                        @else
+                        <a href="{{ route('login') }}"
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
                             Вход
                         </a>
-                        <a href="https://php-task-manager-ru.hexlet.app/register"
+
+                        <a href=" {{ route('register') }} "
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
                             Регистрация
                         </a>
+                        @endauth
                     </div>
 
                     <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <a href="https://php-task-manager-ru.hexlet.app/tasks"
-                                    class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
                                     Задачи </a>
                             </li>
                             <li>
-                                <a href="https://php-task-manager-ru.hexlet.app/task_statuses"
-                                    class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
                                     Статусы </a>
                             </li>
                             <li>
-                                <a href="https://php-task-manager-ru.hexlet.app/labels"
-                                    class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
                                     Метки </a>
                             </li>
                         </ul>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.main');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
