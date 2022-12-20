2711e17e1f2ce6d9f5ef93c1fc9386f9798d5d4b
Roman Burko <redstone.kem@gmail.com>
2022-12-20 18:33:05 +0300

    added validation messages

--- resources/views/taskStatus/create.blade.php
+++ resources/views/taskStatus/create.blade.php
@@ -8,8 +8,7 @@
             <div class="grid col-span-full">
                 <h1 class="mb-5">Создать статус</h1>
 
-                <form method="POST" action="{{ route('task_statuses.store') }}" accept-charset="UTF-8"
-                    class="w-50">
+                <form method="POST" action="{{ route('task_statuses.store') }}" accept-charset="UTF-8" class="w-50">
                     @csrf
                     <div class="flex flex-col">
                         <div>
@@ -18,6 +17,13 @@
                         <div class="mt-2">
                             <input class="rounded border-gray-300 w-1/3" name="name" type="text" id="name" />
                         </div>
+                        @if ($errors->get('name'))
+                            @foreach ($errors->get('name') as $error)
+                                <div class="text-rose-600">
+                                    {{ $error }}
+                                </div>
+                            @endforeach
+                        @endif
                         <div class="mt-2">
                             <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                 type="submit" value="Создать" />
--- resources/views/taskStatus/edit.blade.php
+++ resources/views/taskStatus/edit.blade.php
@@ -16,9 +16,16 @@
                             <label for="name">Имя</label>
                         </div>
                         <div class="mt-2">
-                            <input class="rounded border-gray-300 w-1/3" name="name" type="text" value="{{ $taskStatus->name }}"
-                                id="name">
+                            <input class="rounded border-gray-300 w-1/3" name="name" type="text"
+                                value="{{ $taskStatus->name }}" id="name">
                         </div>
+                        @if ($errors->get('name'))
+                            @foreach ($errors->get('name') as $error)
+                                <div class="text-rose-600">
+                                    {{ $error }}
+                                </div>
+                            @endforeach
+                        @endif
                         <div class="mt-2">
                             <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                 type="submit" value="Обновить">
